6a4788fd426362fb923d2b0849ae5f72bfc94ca0
InstantChiko <hj-G20016@sist.ac.jp>
2021-04-27 12:27:49 +0900

    ウィンドウの調節、リソース追加

--- main.cpp
+++ main.cpp
@@ -1,18 +1,48 @@
-#include "DxLib.h"
+//ヘッダーファイルの読み込み
+#include "DxLib.h"	//DxLibを使う時は必要
+#include <stdio.h>
+
+//マクロ定義
+#define GAME_TITLE	"ゲームタイトル"	//ゲームタイトル（お先まっくら）
+#define GAME_WIDTH	1280				//ゲーム画面の幅（ウィドス）
+#define GAME_HEIGHT	720					//ゲーム画面の高さ（ハイト）
+#define GAME_COLOR	32					//ゲームの色域
+
 
 // プログラムは WinMain から始まります
+//Windowsのプログラム方法（WinAPI）で動いている
+//DxLibは、DirectXという、ゲームプログラミングを簡単に使える仕組み
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	ChangeWindowMode(TRUE);					//ウィンドウモードに設定
+	SetMainWindowText("お先まっくら");	//ウィンドウのタイトルの文字
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//ウィンドウの解像度を設定
+	SetWindowSize(GAME_WIDTH, GAME_HEIGHT);		//ウィンドウのの大きさを設定
+	SetBackgroundColor(255, 255,255);			//デフォルトの背景色
+	
+
+	// ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1)		
 	{
 		return -1;			// エラーが起きたら直ちに終了
 	}
 
-	DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
+	//無限ループ
+	while (1)
+	{
+		if (CheckHitKeyAll() != 0)
+		{
+			break;	//無限ループを抜ける
+		}
+		//メッセージを受け取り続ける
+		if (ProcessMessage() != 0)	//-1のとき、エラーやウィンドウが閉じられた
+		{
+			break;	//無限ループを抜ける
+		}
+	}
 
-	WaitKey();				// キー入力待ち
-
-	DxLib_End();				// ＤＸライブラリ使用の終了処理
-
+	// ＤＸライブラリ使用の終了処理
+	DxLib_End();
 	return 0;				// ソフトの終了 
 }
